0d6ed6451e9089055c079ac07966a7e4aa92402d
Edwin <lldevans@protonmail.com>
2025-05-17 15:58:31 -0400

    Update cleaning_standardization.sql

--- scripts/silver/cleaning_standardization.sql
+++ scripts/silver/cleaning_standardization.sql
@@ -1,1 +1,25 @@
+/*
+These tests were applied as follows
+Null and Duplicates ==> cst_id
+Unwanted Spaces ==> cst_firstname, cst_lastname, cst_gndr and cst_marital_status
+Full meanings and n/a ==> cst_gndr and cst_marital_status
+*/
+
+--Check for Nulls or Duplicates in Primary Key
+--Expectation: No results
+
+SELECT cst_id, COUNT (*)
+FROM bronze.crm_cust_info
+group by cst_id
+having count (*) > 1 OR cst_id IS NULL
+
+-- check for unwanted spaces
+-- Expectation: No Results
+SELECT cst_firstname
+FROM bronze.crm_cust_info
+WHERE cst_firstname != TRIM (cst_firstname)
+
+-- Data Standardization & Consistency
+SELECT DISTINCT cst_marital_status
+from bronze.crm_cust_info
 
