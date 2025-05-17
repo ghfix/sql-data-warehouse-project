4c7e76260967a6da6b1d043df5c9eda7de335fb6
Edwin <lldevans@protonmail.com>
2025-05-17 16:12:46 -0400

    Update crm_cust_info.sql

--- scripts/silver/crm_cust_info.sql
+++ scripts/silver/crm_cust_info.sql
@@ -15,13 +15,15 @@ SELECT
 	cst_key,
 	TRIM(cst_firstname) AS cst_firstname,
 	TRIM(cst_lastname) AS cst_lastname,
-	CASE WHEN	UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
-		 WHEN	UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'	
-		 ELSE	'n/a'
+	CASE 
+		WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+		WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'	
+		ELSE 'n/a'
 	END AS cst_marital_status, -- Normalize marital status values to readable format
-	CASE WHEN	UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
-		 WHEN	UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'	
-		 ELSE	'n/a'
+	CASE 
+		WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+		WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'	
+		ELSE 'n/a'
 	END AS	cst_gndr, -- Normalize gender values to readable format
 	cst_create_date
 FROM 
