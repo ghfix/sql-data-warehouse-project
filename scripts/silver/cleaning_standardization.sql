aebca0c509e51360cbac4264d90b686f78cd9db4
Edwin <lldevans@protonmail.com>
2025-05-18 13:54:06 -0400

    Update cleaning_standardization.sql

--- scripts/silver/cleaning_standardization.sql
+++ scripts/silver/cleaning_standardization.sql
@@ -61,3 +61,44 @@ FROM silver.crm_prd_info
 WHERE prd_end_dt < prd_start_dt
 
 
+--Check for invalid dates
+
+SELECT
+	NULLIF (sls_due_dt , 0) sls_due_dt
+FROM 
+	silver.crm_sales_details
+WHERE 
+	sls_due_dt <= 0 
+OR LEN(sls_due_dt) != 8
+OR sls_due_dt > 20500101
+OR sls_due_dt < 19000101
+
+-- Check for Invalid Date Orders
+select * from silver.crm_sales_details
+where sls_order_dt > sls_ship_dt OR sls_ship_dt > sls_due_dt
+
+
+
+=================================
+crm sales details quality checks
+=================================
+--Check Data Consistency: Between Sales, Quantity, and Price
+-- >> Sales = Quantity * Price
+-- >> Values must not be NULL, zero, or negative.
+
+--Rules
+-- If Sales is negative, zero, or null, derive it using Quantity and Price
+-- If Price is zero or null, calculate it using Sales and Quantity
+-- If price is negative, convert it to a positive value
+SELECT DISTINCT
+sls_sales,
+sls_quantity,
+sls_price
+FROM silver.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price
+OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0
+ORDER BY sls_sales, sls_quantity, sls_price
+
+select * from silver.crm_sales_details
+
