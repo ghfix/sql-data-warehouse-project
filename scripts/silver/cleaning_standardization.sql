042250674c0c82c202eb2ca973b741c5f9b4d290
Edwin <lldevans@protonmail.com>
2025-05-18 07:06:55 -0400

    Update cleaning_standardization.sql

--- scripts/silver/cleaning_standardization.sql
+++ scripts/silver/cleaning_standardization.sql
@@ -1,4 +1,7 @@
 /*
+============================
+crm_cust_info
+============================
 These tests were applied as follows
 Null and Duplicates ==> cst_id
 Unwanted Spaces ==> cst_firstname, cst_lastname, cst_gndr and cst_marital_status
@@ -22,4 +25,39 @@ WHERE cst_firstname != TRIM (cst_firstname)
 -- Data Standardization & Consistency
 SELECT DISTINCT cst_marital_status
 from bronze.crm_cust_info
+
+/*
+============================
+crm_prd_info
+============================
+*/
+  
+-- Check for Nulls or Duplicates in Primary Key
+-- Expectation: No Result
+select prd_id, count (*)
+from silver.crm_prd_info
+group by prd_id
+having count (*) > 1 or prd_id is null
+
+-- Check for unwanted spaces
+-- Expectation: No Result
+select prd_nm
+from silver.crm_prd_info
+where prd_nm != TRIM(prd_nm)  
+
+-- Check for Nulls or Negative Numbers
+-- Expectation: No results
+Select prd_cost
+FROM silver.crm_prd_info
+where prd_cost < 0 OR prd_cost IS NULL
+
+-- Data Standardization & Consistency
+SELECT DISTINCT prd_line
+FROM silver.crm_prd_info
+
+-- Check for Invalid Date Orders
+SELECT *
+FROM silver.crm_prd_info
+WHERE prd_end_dt < prd_start_dt
+
 
