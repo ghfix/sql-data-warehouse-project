b821e31d5f23d4a36999756b41fae65002d66095
Edwin <lldevans@protonmail.com>
2025-05-18 07:44:39 -0400

    Update init__database.sql

--- scripts/bronze/init__database.sql
+++ scripts/bronze/init__database.sql
@@ -49,7 +49,7 @@ Creating Tables
 
 IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_cust_info;
-	
+GO
 CREATE TABLE bronze.crm_cust_info (
 	cst_id INT,
 	cst_key NVARCHAR (50),
@@ -62,7 +62,7 @@ CREATE TABLE bronze.crm_cust_info (
 
 IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_prd_info;
-
+GO
 CREATE TABLE bronze.crm_prd_info (
 	prd_id INT,
 	prd_key NVARCHAR (50),
@@ -74,14 +74,14 @@ CREATE TABLE bronze.crm_prd_info (
 );
 IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_sales_details;
-
+GO
 CREATE TABLE bronze.crm_sales_details (
 	sls_ord_num NVARCHAR (50),
 	sls_prd_key NVARCHAR (50),
 	sls_cust_id INT,
-	sls_order_dt DATE,
-	sls_ship_dt DATE,
-	sls_due_dt DATE,
+	sls_order_dt INT,
+	sls_ship_dt INT,
+	sls_due_dt INT,
 	sls_sales INT,
 	sls_quantity INT,
 	sls_price INT
@@ -89,7 +89,7 @@ CREATE TABLE bronze.crm_sales_details (
 
 IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_cust_az12;
-
+GO
 CREATE TABLE bronze.erp_cust_az12(
 	cid NVARCHAR (100),
 	bdate DATE,
@@ -98,7 +98,7 @@ CREATE TABLE bronze.erp_cust_az12(
 
 IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_loc_a101;
-
+GO
 CREATE TABLE bronze.erp_loc_a101 (
 	cid NVARCHAR (50),
 	cntry NVARCHAR (50)
@@ -106,7 +106,7 @@ CREATE TABLE bronze.erp_loc_a101 (
 
 IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_px_cat_g1v2;
-
+GO
 CREATE TABLE bronze.erp_px_cat_g1v2 (
 	id NVARCHAR (50),
 	cat VARCHAR (50),
