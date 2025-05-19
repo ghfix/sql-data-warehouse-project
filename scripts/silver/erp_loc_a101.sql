5a2f4ab4c2efb6b9545d8f1b5c2fd88e406b0e5b
Edwin <lldevans@protonmail.com>
2025-05-19 08:16:50 -0400

    Create erp_loc_a101.sql

---
+++ scripts/silver/erp_loc_a101.sql
@@ -0,0 +1,18 @@
+===================================================================
+Inserting erp_loc_a101 from the bronze layer into the silver layer
+===================================================================
+
+INSERT INTO silver.erp_loc_a101 (
+	cid,
+	cntry
+)
+SELECT 
+	REPLACE (cid, '-','') cid,
+CASE
+	WHEN TRIM(cntry) = 'DE'THEN 'Germany'
+	WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+	WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+	ELSE TRIM (cntry)
+END AS cntry
+FROM 
+	bronze.erp_loc_a101
