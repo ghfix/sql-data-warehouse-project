a356eb804301f509b8c831b48ee9d7c0f898701f
Edwin <lldevans@protonmail.com>
2025-05-19 08:17:41 -0400

    Update erp_loc_a101.sql

--- scripts/silver/erp_loc_a101.sql
+++ scripts/silver/erp_loc_a101.sql
@@ -13,6 +13,6 @@ CASE
 	WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
 	WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
 	ELSE TRIM (cntry)
-END AS cntry
+END AS cntry -- Normalize and Handle missing or blank country codes
 FROM 
 	bronze.erp_loc_a101
