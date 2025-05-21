78689d435070b3f662de493d43ec594d2b0958a2
Edwin <lldevans@protonmail.com>
2025-05-20 23:32:48 -0400

    Update Quality-checks.sql

--- scripts/gold/Quality-checks.sql
+++ scripts/gold/Quality-checks.sql
@@ -1,7 +1,6 @@
-========================================
-Using subqueries to check for duplicates
-========================================
-
+=================================================
+Using subqueries to check for customer duplicates
+=================================================
 SELECT cst_id, COUNT(*) FROM
 	(SELECT 
 		ci.cst_id,
@@ -26,3 +25,29 @@ SELECT cst_id, COUNT(*) FROM
 		ci.cst_key = la.cid
 )t GROUP BY cst_id
 HAVING COUNT (*) > 1
+
+
+
+=================================================
+Using subqueries to check for product duplicates
+=================================================
+select prd_id, count(*) from
+(select 
+	pn.prd_id,
+	pn.cat_id,
+	pn.prd_key,
+	pn.prd_nm,
+	pn.prd_cost,
+	pn.prd_line,
+	pn.prd_start_dt,
+	pc.cat,
+	pc.subcat,
+	pc.maintenance
+from 
+	silver.crm_prd_info pn  
+left join silver.erp_px_cat_g1v2 pc
+on pn.cat_id = pc.id
+where prd_end_dt IS NULL -- Filter out all historical data
+)t
+group by prd_id
+having count (*) > 1
