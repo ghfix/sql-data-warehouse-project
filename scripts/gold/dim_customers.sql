addfce8deec962c2cdbaae7878e6faa35c1c7a00
Edwin <lldevans@protonmail.com>
2025-05-23 17:21:48 -0400

    Update dim_customers.sql

--- scripts/gold/dim_customers.sql
+++ scripts/gold/dim_customers.sql
@@ -5,18 +5,19 @@ Putting all the clean customer information together in the gold layer
 
 CREATE VIEW gold.dim_customers AS
 SELECT
-	ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key,
-	ci.cst_id AS customer_id,
-	ci.cst_key AS customer_number,
-	ci.cst_firstname AS first_name,
-	ci.cst_lastname AS last_name,
-	la.cntry AS country,
-	ci.cst_marital_status AS marital_status,
-	CASE WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- CRM is the Master for gender Info
-		ELSE COALESCE (ca.gen, 'n/a')
-	END AS gender,
-	ca.bdate AS birthdate,
-	ci.cst_create_date AS create_date
+	ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key, -- Surrogate Key
+	ci.cst_id 			AS customer_id,
+	ci.cst_key 			AS customer_number,
+	ci.cst_firstname 		AS first_name,
+	ci.cst_lastname 		AS last_name,
+	la.cntry 			AS country,
+	ci.cst_marital_status 		AS marital_status,
+	CASE 
+		WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- CRM is the Master for gender Info
+		ELSE COALESCE (ca.gen, 'n/a')		   -- Fallback to ERP Data
+	END 				AS gender,
+	ca.bdate 			AS birthdate,
+	ci.cst_create_date 		AS create_date
 	
 FROM 
 	silver.crm_cust_info ci
